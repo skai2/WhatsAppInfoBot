480e2ec3e9a3d4b8b87589ba582f71b5c62b4436
SKai <cpiantino@gmail.com>
2018-11-01 02:20:42 +0000

    Update script to always update uaibot code on start

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 echo "--------------------------"
 echo "Starting UaiBot"
 echo "--------------------------"
-# Change directory and start mac
+# Change directory to update module
 cd whatsapp-framework
+# Copy uaibot to mac modules
+rm -f modules/uaibot/uaibot.py
+cp ../uaibot.py modules/uaibot/uaibot.py
+# Start mac
 . ./start.sh
