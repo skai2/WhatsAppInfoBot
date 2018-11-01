aa17a1d504d483010c44f7ec2633fc86d51953dc
SKai <cpiantino@gmail.com>
2018-11-01 02:07:27 +0000

    Fix module copied to wrong directory

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 echo "Starting UaiBot"
 # Copy uaibot to mac modules
-cp uaibot.py whatsapp-framework/uaibot.py
+rm -f whatsapp-framework/modules/uaibot.py
+cp uaibot.py whatsapp-framework/modules/uaibot.py
 # Change directory and start mac
 cd whatsapp-framework
 . ./start.sh
